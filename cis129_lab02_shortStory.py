76348390eeb8e1aee4a1779c3112ab5d4cab098b
Nicolas Velasco <159108705+Minion3k@users.noreply.github.com>
2024-02-07 13:39:41 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,5 @@
-print('Hello, World')
+#Author: Nicolas Velasco
+print("Today was a good day today.")
+print("I woke up ate breakfast, went to school, went to work, and came home whithout any struggle.")
+print("Now I get to sit and relax for the rest of the night.")
+print("Before I get ready for bed im going to eat pizza and watch T.V.")
