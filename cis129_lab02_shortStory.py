7bb752d3cd2518347ca4aefd5c1341ace28c629c
Nicolas Velasco <159108705+Minion3k@users.noreply.github.com>
2024-02-07 23:49:48 -0700

    Update cis129_lab02_shortStory.py
    I made a short story of my day

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,5 +1,9 @@
 #Author: Nicolas Velasco
-print("Today was a good day today.")
-print("I woke up ate breakfast, went to school, went to work, and came home whithout any struggle.")
-print("Now I get to sit and relax for the rest of the night.")
-print("Before I get ready for bed im going to eat pizza and watch T.V.")
+#CIS129 lab 2
+#Short story
+print("Today was a very intresting day today.")
+print("I woke up ate breakfast, and took my brother and sister to school.")
+print("I then went to school which it started to rain and it rained for most of the day.")
+print("When I went to work the power ended up going out so we had to close")
+print("The power did eventually come back on and I did work a full shift")
+print("After that I went home showered and went to bed for the night")
